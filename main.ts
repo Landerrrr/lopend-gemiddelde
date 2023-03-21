ff048aebc7d56378a419cb759e003a322d8d66e2
Landerrrr <125375662+Landerrrr@users.noreply.github.com>
2023-03-21 15:30:20 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,18 +1,33 @@
+function DOEDIT () {
+    metingen = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
+    ]
+    som = 0
+    serial.redirect(
+    SerialPin.USB_TX,
+    SerialPin.USB_RX,
+    BaudRate.BaudRate115200
+    )
+    Laag = 1023
+    Hoog = 0
+}
+let Hoog = 0
+let Laag = 0
+let Gemiddelde = 0
+let som = 0
+let metingen: number[] = []
 let LaatsteMeting = 0
 let meting = 0
-let metingen = [
-0,
-0,
-0,
-0,
-0,
-0,
-0,
-0,
-0,
-0
-]
-let som = 0
+DOEDIT()
 while (true) {
     meting = pins.analogReadPin(AnalogPin.P0)
     LaatsteMeting = metingen[0]
@@ -20,7 +35,15 @@ while (true) {
     som += -1 * LaatsteMeting
     metingen.push(meting)
     metingen.removeAt(0)
+    Gemiddelde = som / 10
+    if (Gemiddelde < Laag) {
+        Laag = Gemiddelde
+    }
+    if (Gemiddelde > Hoog) {
+        Hoog = Gemiddelde
+    }
+    basic.pause(5000)
+    serial.writeLine("" + Laag + "   -   " + Hoog)
+    Laag = 1023
+    Hoog = 0
 }
-basic.forever(function () {
-	
-})
